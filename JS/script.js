9db392da7202aae0e0d27eeb66f59a81c1af2aa2
Jes Sch <jesica_schaffer@hotmail.com>
2022-12-04 20:26:00 -0300

    versión final con fetch desde mi data.json

--- JS/script.js
+++ JS/script.js
@@ -9,39 +9,12 @@ inputValorInflacion = document.getElementById("inputValorInflacion"),
 btnRecuperarUltimo = document.getElementById("btnCalcular"),
 inputPrecioContado = document.getElementById("inputPrecioContado");
 
-/* Luego de adquirir el TOKEN agregue el siguiente Header a sus requests:
-Authorization: BEARER {TOKEN} */
-
-const APITOKEN = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDE1NDIxNTMsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJqZXNpY2Euc2NoYWZmZXJAZ21haWwuY29tIn0.DsWImkUNEBqsXLyUT2cShXDnnzbfb84ljKoO8gH8UTkYX-QXjEuPGK0nlTq_Utecv-FOFLJvb7EoA3UMDpZyrA';
-
-const urlDolarSi = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales';
-const url = 'https://api.estadisticasbcra.com/inflacion_mensual_oficial';
-
-/* fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-.then(response=>response.json())
-.then(data=>console.log(data)) */
-/* 
-var xmlHttpRequest1 = new XMLHttpRequest();
-xmlHttpRequest1.open("GET", url, true);
-xmlHttpRequest1.setRequestHeader('Access-Control-Allow-Origin', 'https://estadisticasbcra.com');
-xmlHttpRequest1.setRequestHeader('Access-Control-Allow-Headers', 'application/json; charset=UTF-8');
-xmlHttpRequest1.setRequestHeader('Access-Control-Allow-Methods', 'OPTIONS,POST,GET');
-xmlHttpRequest1.setRequestHeader('Authorization', 'Bearer ' + APITOKEN);
-
-console.log(xmlHttpRequest1.send()); */
 document.onreadystatechange = async function()
 {
     if (document.readyState === 'complete')
     {
-        //dom is ready, window.onload fires later
         let inflacionData = await fetch ('./JS/data.json')
-        let d = new Date();
-        d.setDate(0); //sets d to the last day of the previous month
-
-        //let resultObject = ObtenerUltimo(await inflacionData.json()) //search(formatDate(d), await inflacionData.json());
-
         inputValorInflacion.value = ObtenerUltimo(await inflacionData.json());
-
     }
 };
 
@@ -50,64 +23,6 @@ function ObtenerUltimo(myArray){
 
     return ordenado.at(-1).v;
 }
-
-function formatDate(date) {
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
-
-    if (month.length < 2) 
-        month = '0' + month;
-    if (day.length < 2) 
-        day = '0' + day;
-
-    return [year, month, day].join('-');
-}
-
-function search(nameKey, myArray){
-    for (let i=0; i < myArray.length; i++) {
-        if (myArray[i].d === nameKey) {
-            return myArray[i].v;
-        }
-    }
-}
-
-/* const usarJson = async function () {
-    let response = await fetch('./js/data.json');
-    let productos = await response.json();
-} */
-
-/* fetch(urlDolarSi)
-		.then(response => response.text())
-		.then(response => console.log(response)); */
-/* fetch('https://api.estadisticasbcra.com/inflacion_mensual_oficial')
-.then(response=>response.json())
-.then(data=>console.log(data)) */
-
-// headers you pass to a http request
-/* let header = 'Authorization: BEARER {eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDE1NDIxNTMsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJqZXNpY2Euc2NoYWZmZXJAZ21haWwuY29tIn0.DsWImkUNEBqsXLyUT2cShXDnnzbfb84ljKoO8gH8UTkYX-QXjEuPGK0nlTq_Utecv-FOFLJvb7EoA3UMDpZyrA}'; */
-
-/* fetch(url, {
-  method: 'GET',
-  body: JSON.stringify(data),
-  header:{
-    Authorization: BEARER eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDE1NDIxNTMsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJqZXNpY2Euc2NoYWZmZXJAZ21haWwuY29tIn0.DsWImkUNEBqsXLyUT2cShXDnnzbfb84ljKoO8gH8UTkYX-QXjEuPGK0nlTq_Utecv-FOFLJvb7EoA3UMDpZyrA
-  }
-})
-.then(response=>response.json())
-.then(data=>console.log(data)) */
-
-
-/* fetch("https://api.estadisticasbcra.com/inflacion_mensual_oficial",
-{
-   "method":"GET",
-   "body":"JSON.stringify(data)",
-   "header":{
-      "Authorization: BEARER {APITOKEN}"
-   }
-}).then(response=>response.json())
-.then(data=>console.log(data)) */
 
 const modal = () => {
     let exampleModal = document.getElementById("exampleModal");
@@ -185,7 +100,6 @@ function calcular() {
             text: "Tenés que cargar todos los datos para poder CALCULAR los resultados ;)",
             imageUrl: "https://www.losandes.com.ar/resizer/z3rmrT-vh1VF9hNjOzJ0-3cwS4E=/980x640/smart/cloudfront-us-east-1.images.arcpublishing.com/grupoclarin/L4UFWTYTTZEHZBMPAVJLGKBBYA.png",
             imageWidth: 450,
-            //imageHeigth: 200,
             imageAlt: "Pepe Argento haciendo caras",
             width: 500,
             color: "#1124f2",
